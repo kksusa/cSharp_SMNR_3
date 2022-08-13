21ffc6223a5570a7afcb236eaf4c26d7fedc8d3b
kksusa <kksusa1@gmail.com>
2022-08-13 18:24:15 +0500

    Задание 2

--- TSK_2/Program.cs
+++ TSK_2/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿Console.WriteLine("Напишите координату Х точки А:");
+string? xAstring = Console.ReadLine();
+int xA = int.Parse(xAstring!);
+
+Console.WriteLine("Напишите координату Y точки А:");
+string? yAstring = Console.ReadLine();
+int yA = int.Parse(yAstring!);
+
+Console.WriteLine("Напишите координату Z точки А:");
+string? zAstring = Console.ReadLine();
+int zA = int.Parse(zAstring!);
+
+Console.WriteLine("Напишите координату Х точки B:");
+string? xBstring = Console.ReadLine();
+int xB = int.Parse(xBstring!);
+
+Console.WriteLine("Напишите координату Y точки B:");
+string? yBstring = Console.ReadLine();
+int yB = int.Parse(yBstring!);
+
+Console.WriteLine("Напишите координату Z точки B:");
+string? zBstring = Console.ReadLine();
+int zB = int.Parse(zBstring!);
+
+double vectorLength = Math.Round(Math.Sqrt(Math.Pow((xB - xA), 2) + Math.Pow((yB - yA), 2) + Math.Pow((zB - zA), 2)), 2);
+Console.WriteLine("Длина вектора в трёхмерном пространстве равна " + vectorLength + ".");
